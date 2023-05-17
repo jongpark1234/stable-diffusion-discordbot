01949ec039602081145891b80fc45d3aa3812db8
JongPark <jongpark1234@naver.com>
2023-05-17 14:18:15 +0900

    :recycle: Refactor code
    + Change normal discord.py function to Customized function

--- Cogs/memory.py
+++ Cogs/memory.py
@@ -5,6 +5,7 @@ import requests
 import os
 
 from config import *
+from Functions.send_message import send_message
 
 class memory(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -15,8 +16,9 @@ class memory(commands.Cog):
         '''현재 서버 컴퓨터의 RAM과 VRAM 사용량을 알려 줍니다.'''
 
         data = json.loads(requests.get(url=f'{APIURL}/sdapi/v1/memory').text)
-        await interaction.response.send_message(
-            f'''
+        await send_message(
+            interaction=interaction,
+            content=f'''
         GPU : {os.popen('wmic path win32_VideoController get name').read()[12:].strip()}
 
         사용 가능 RAM: **{data['ram']['free'] / 1e+9:.1f}GB**
