c308e98cf3e2487226eab4afbbe37615031fede0
JongPark <jongpark1234@naver.com>
2023-05-17 14:46:49 +0900

    ♻️ Refactor code
    + Change normal discord.py function to Customized function

--- Cogs/get-options.py
+++ Cogs/get-options.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import json
 
 from config import *
+from Functions.send_message import send_message
 
 class get_options(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,13 +12,17 @@ class get_options(commands.Cog):
     @discord.app_commands.command(name="get-options")
     async def get_options(self, interaction: discord.Interaction):
         '''현재 설정되있는 모델과 vae를 알려 줍니다.'''
-        json_data = json.load(open(JSONPATH, 'r'))
-        for user in json_data['users']:
+        for user in json.load(open(JSONPATH, 'r'))['users']:
             if interaction.user.id == user['userid']:
-                await interaction.response.send_message(f"<@{interaction.user.id}> 님의 설정값\n\nModel_Checkpoint : **{user['model']}**\nVAE : **kl-f8-anime2.ckpt**")
+                await send_message(
+                    interaction=interaction,
+                    content=f'<@{interaction.user.id}> 님의 설정값\n\nModel_Checkpoint : **{user["model"]}**\nVAE : **kl-f8-anime2.ckpt**'
+                )
                 break
         else:
-            await interaction.response.send_message(f"<@{interaction.user.id}>님은 아직 모델 설정을 하지 않으신것 같아요!\n **/set-model** 으로 설정해주세요.")
+            await interaction.response.send_message(
+                f'<@{interaction.user.id}>님은 아직 모델 설정을 하지 않으신것 같아요!\n **/set-model** 으로 설정해주세요.'
+            )
 
 
 async def setup(bot: commands.Bot):
