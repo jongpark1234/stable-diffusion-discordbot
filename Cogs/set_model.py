7b2b594c4de607549cdbbc807e8fc791e544fbcf
JongPark <jongpark1234@naver.com>
2023-05-17 14:42:25 +0900

    :recycle: Refactor code
    + Change normal discord.py function to Customized function

--- Cogs/set_model.py
+++ Cogs/set_model.py
@@ -5,38 +5,41 @@ from discord import ButtonStyle
 import json
 import requests
 import math
+
 from config import *
+from Functions.send_message import send_message
+from Functions.edit_message import edit_message
+from Functions.edit_original_response import edit_original_response
 
 is_selected = {}
 
 class set_model(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.curPage = 1
+
+    def generateSelector(self, fileList: list[str], fileSize: int) -> list[discord.SelectOption]:
+        return [
+            discord.SelectOption(
+                label=file['model_name'],
+                description=file['hash']
+            ) for file in fileList
+        ][(self.curPage - 1) * 25:min(self.curPage * 25, fileSize)]
 
     @discord.app_commands.command(name='set-model')
     async def set_model(self, interaction: discord.Interaction):
         '''Change Model.'''
-        global curPage, is_selected
+        global is_selected
         is_selected[interaction.user.id] = False
         modelList = json.loads(requests.get(url=f'{APIURL}/sdapi/v1/sd-models').text)
         modelCount = len(modelList)
         pageLimit = math.ceil(modelCount / 25)
-        curPage = 1
         
-        def generateSelector():
-            return [
-                discord.SelectOption(
-                    label=f'{modelList[i]["model_name"]}',
-                    description=f'{modelList[i]["hash"]}'
-                ) for i in range((curPage - 1) * 25, min(curPage * 25, modelCount)) # available maxValue is modelCount.
-            ]
-
-
         # UI 선언 및 콜백 함수
-        selects = Select(options=generateSelector())
+        selects = Select(options=self.generateSelector(modelList, modelCount))
         left = Button(label='<<', style=ButtonStyle.primary)
         right = Button(label='>>', style=ButtonStyle.primary)
-        page = Button(label = f'{curPage}/{pageLimit}', style=ButtonStyle.grey, disabled=True)
+        page = Button(label = f'{self.curPage}/{pageLimit}', style=ButtonStyle.grey, disabled=True)
         approve = Button(label='✅', style=ButtonStyle.green)
         cancel = Button(label='X', style=ButtonStyle.red)
         
@@ -47,36 +50,58 @@ class set_model(commands.Cog):
             embed.set_author(name=f'{is_selected[interaction.user.id]} 모델로 바꿀까요?')
             embed.set_footer(text='@DavidChoi#6516')
             try:
-                res = discord.File(f"{MODELPATH}\\{is_selected[interaction.user.id]}.png", filename=f"{is_selected[interaction.user.id]}.png")
+                modelCover = discord.File(f"{MODELPATH}\\{is_selected[interaction.user.id]}.png", filename=f"{is_selected[interaction.user.id]}.png")
                 embed.set_image(url=f"attachment://{is_selected[interaction.user.id]}.png")
-                await interaction.response.edit_message(view=view_make(), embed=embed, attachments=[res])
+                await edit_message(
+                    interaction=interaction,
+                    embed=embed,
+                    attachments=[modelCover],
+                    view=view_make(),
+                )
             except:
-                await interaction.response.edit_message(view=view_make(), embed=embed)
+                await edit_message(
+                    interaction=interaction,
+                    embed=embed,
+                    attachments=[],
+                    view=view_make()
+                )
 
         async def left_callback(interaction: discord.Interaction):
-            global curPage
-            curPage = max(curPage - 1, 1)
-            await interaction.response.edit_message(view=view_make())
+            self.curPage = max(self.curPage - 1, 1)
+            await edit_message(
+                interaction=interaction,
+                view=view_make()
+            )
 
         async def right_callback(interaction: discord.Interaction):
-            global curPage
-            curPage = min(curPage + 1, pageLimit)
-            await interaction.response.edit_message(view=view_make())
+            self.curPage = min(self.curPage + 1, pageLimit)
+            await edit_message(
+                interaction=interaction,
+                view=view_make()
+            )
 
         async def page_callback(interaction: discord.Interaction):
             global is_selected
             is_selected[interaction.user.id] = False
-            await interaction.response.edit_message(content='Todo: 페이지 한번에 이동하기', view=view_make())
+            await edit_message(
+                interaction=interaction,
+                view=view_make()
+            )
 
         async def approve_callback(interaction: discord.Interaction):
             embed = discord.Embed(title=f'**{is_selected[interaction.user.id]}** 모델로 설정합니다.', color=0x777777)
             embed.set_footer(text='@DavidChoi#6516')
-            await interaction.response.edit_message(embed=embed, view=View(), attachments=[])
+            await edit_message(
+                interaction=interaction,
+                embed=embed,
+                attachments=[],
+                view=View()
+            )
 
             json_data = json.load(open(JSONPATH, 'r'))
-            user_cnt = len(json_data['users'])
+            userCount = len(json_data['users'])
 
-            for i in range(user_cnt):
+            for i in range(userCount):
                 if interaction.user.id == json_data['users'][i]['userid']:
                     json_data['users'][i]['model'] = is_selected[interaction.user.id]
                     break
@@ -86,16 +111,18 @@ class set_model(commands.Cog):
                     'model': is_selected[interaction.user.id]
                 })
             
-            with open(JSONPATH, 'w') as outfile:
-                json.dump(json_data, outfile, indent=4)
-            embed.title = f'**{is_selected[interaction.user.id]}** 모델로 설정했어요!'
+            json.dump(json_data, open(JSONPATH, 'w'), indent=4)
             is_selected.pop(interaction.user.id)
-            await interaction.edit_original_response(embed=embed)
 
         async def cancel_callback(interaction : discord.Interaction):
-            embed=discord.Embed(title=f"모델 변경을 취소했어요.", color=0xca474c)
+            embed=discord.Embed(title=f'모델 변경을 취소했어요.', color=0xca474c)
             embed.set_footer(text='@DavidChoi#6516')
-            await interaction.response.edit_message(view=View(), embed=embed, attachments=[])
+            await edit_message(
+                interaction=interaction,
+                embed=embed,
+                attachments=[],
+                view=View(),
+            )
 
         selects.callback = select_callback
         left.callback = left_callback
@@ -107,21 +134,25 @@ class set_model(commands.Cog):
         def view_make():
             view = View()
 
-            left.disabled = curPage == 1
-            right.disabled = curPage == pageLimit
+            left.disabled = self.curPage == 1
+            right.disabled = self.curPage == pageLimit
             approve.disabled = is_selected[interaction.user.id] == False
-            page.label = f'{curPage} / {pageLimit}'
+            page.label = f'{self.curPage} / {pageLimit}'
 
-            selects.options = generateSelector()
+            selects.options = self.generateSelector(modelList, modelCount)
 
             for ui in [selects, left, page, right, approve, cancel]:
                 view.add_item(ui)
                 
             return view
 
-        embed=discord.Embed(title=f"아래 선택 메뉴에서 모델을 골라 주세요", color=0x777777)
+        embed=discord.Embed(title='아래 선택 메뉴에서 모델을 골라 주세요', color=0x777777)
         embed.set_footer(text='@DavidChoi#6516')
-        await interaction.response.send_message(embed=embed, view=view_make())
+        await send_message(
+            interaction=interaction,
+            embed=embed,
+            view=view_make()
+        )
 
 
 async def setup(bot: commands.Bot):
